cef9d6b40b9f72002a70ac7806dc9ed671014f1e
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-02-13 15:09:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-     int i=min(a,b);
+     int i=0;
+     if (a>b) i=b;
+     else i=a;
     while (i>0)
     {
         if ((a%i==0)&&(b%i==0)) 
