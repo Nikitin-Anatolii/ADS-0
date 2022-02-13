c82af05e875cdab03889f8f1af0aa030cc05bd13
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-02-13 16:09:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,12 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) 
-{
-     int i=0;
-     if (a>b) i=b;
-     else i=a;
-    while (i>0)
-    {
-        if ((a%i==0)&&(b%i==0)) 
-        {
-        return i;
+int gcd(int a, int b){
+    while ((a != 0) && (b != 0)) {
+        if (a > b) {
+            a = a % b;
+        } else {
+            b = b % a;
         }
-        else --i;
     }
+    return (a + b);
 }
